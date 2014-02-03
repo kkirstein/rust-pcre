130cd4146dc323e161d05a8dd11521436a8ad42f
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2014-02-03 17:03:19 +0100

    add basic compile & study tests

--- src/pcre/test.rs
+++ src/pcre/test.rs
@@ -11,12 +11,40 @@
 #[license = "BSD"];
 
 extern mod pcre;
+use pcre::{compile, study};
 
 // test compilation of regex
 #[test]
-fn test_compile() {
-	let pcre_comp = ::pcre::compile("Hello", pcre::PCRE_NONE);
+fn test_raw_compile() {
+	let pcre_comp = compile("Hello", pcre::PCRE_NONE);
 	assert!(pcre_comp != None);
+	//match pcre_comp {
+	//	Some(pc)	=> {
+	//		assert_eq!(pc.magic_number, 0123);
+	//		assert_eq!(pc.options, 0);
+	//	}
+	//	None		=> assert!(false)
+	//}
 }
+
+// test study of regex
+#[test]
+fn test_raw_study() {
+	let pcre_extra = match compile("Hello", pcre::PCRE_NONE) {
+		Some(pc)	=> study(pc, pcre::PCRE_STUDY_NONE),
+		None		=> None
+	};
+	assert!(pcre_extra != None);
+}
+
+#[test]
+fn test_raw_study_jit() {
+	let pcre_extra = match compile("Hello", pcre::PCRE_NONE) {
+		Some(pc)	=> study(pc, pcre::PCRE_STUDY_JIT_COMPILE),
+		None		=> None
+	};
+	assert!(pcre_extra != None);
+}
+
 
 
