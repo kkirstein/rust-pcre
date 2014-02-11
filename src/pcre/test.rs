a28c611968eda9676e5f43e5baae90c42a92d62e
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2014-02-11 11:03:02 +0100

    add tests for group matching, clean-up use imports

--- src/pcre/test.rs
+++ src/pcre/test.rs
@@ -11,27 +11,24 @@
 #[license = "BSD"];
 
 extern mod pcre;
-use pcre::{compile, study, exec, get_substring};
+//use pcre::{compile, study, exec, get_substring};
 
 // test compilation of regex
 #[test]
 fn test_raw_compile() {
-	let pcre_comp = compile("Hello", pcre::PCRE_NONE);
+	use pcre::raw::{compile, PCRE_NONE};
+
+	let pcre_comp = compile("Hello", PCRE_NONE);
 	assert!(pcre_comp != None);
-	//match pcre_comp {
-	//	Some(pc)	=> {
-	//		assert_eq!(pc.magic_number, 0123);
-	//		assert_eq!(pc.options, 0);
-	//	}
-	//	None		=> assert!(false)
-	//}
 }
 
 // test study of regex
 #[test]
 fn test_raw_study() {
-	let pcre_extra = match compile("Hello", pcre::PCRE_NONE) {
-		Some(pc)	=> study(pc, pcre::PCRE_STUDY_NONE),
+	use pcre::raw::{compile, study, PCRE_NONE, PCRE_STUDY_NONE};
+
+	let pcre_extra = match compile("Hello", PCRE_NONE) {
+		Some(pc)	=> study(pc, PCRE_STUDY_NONE),
 		None		=> None
 	};
 	assert!(pcre_extra != None);
@@ -39,8 +36,10 @@ fn test_raw_study() {
 
 #[test]
 fn test_raw_study_jit() {
-	let pcre_extra = match compile("Hello", pcre::PCRE_NONE) {
-		Some(pc)	=> study(pc, pcre::PCRE_STUDY_JIT_COMPILE),
+	use pcre::raw::{compile, study, PCRE_NONE, PCRE_STUDY_JIT_COMPILE};
+
+	let pcre_extra = match compile("Hello", PCRE_NONE) {
+		Some(pc)	=> study(pc, PCRE_STUDY_JIT_COMPILE),
 		None		=> None
 	};
 	assert!(pcre_extra != None);
@@ -48,68 +47,128 @@ fn test_raw_study_jit() {
 
 // test some basic matching cases
 #[test]
-fn test_raw_match () {
-	let regex = match compile("cat", pcre::PCRE_NONE) { Some(r) => r, None => { assert!(false); return }};
-	let subject = "dog and cat";
-	let mm = exec(regex, std::ptr::null(), subject, 0, pcre::PCRE_NONE, 1);
-	let substring = match get_substring(subject, &mm, 0) {
-		Some(s)	=> s,
-		None	=> { assert!(false); return }
-	};
-	assert!(match mm { pcre::Match(_,_) => true, _ => false });
-	assert_eq!(~"cat", substring);
+fn test_raw_match1 () {
+	use pcre::raw::{compile, exec, get_substring, PCRE_NONE, Match};
 
-	let subject = "catch";
-	let mm = exec(regex, std::ptr::null(), subject, 0, pcre::PCRE_NONE, 1);
+	let regex = match compile("cat", PCRE_NONE) { Some(r) => r, None => fail!() };
+	let subject = "dog and cat";
+	let mm = exec(regex, std::ptr::null(), subject, 0, PCRE_NONE, 1);
 	let substring = match get_substring(subject, &mm, 0) {
 		Some(s)	=> s,
 		None	=> { assert!(false); return }
 	};
-	assert!(match mm { pcre::Match(_,_) => true, _ => false });
+	assert!(match mm { Match(_,_) => true, _ => false });
 	assert_eq!(~"cat", substring);
+}
+
+#[test]
+fn test_raw_match2 () {
+	use pcre::raw::{compile, exec, get_substring, PCRE_NONE, Match};
+
+	let regex = match compile("cat", PCRE_NONE) { Some(r) => r, None => fail!() };
+	let subject = "catch";
+	let mm = exec(regex, std::ptr::null(), subject, 0, PCRE_NONE, 1);
+	let substring = match get_substring(subject, &mm, 0) {
+		Some(s)	=> s,
+		None	=> { assert!(false); return }
+	};
+	assert!(match mm { Match(_,_) => true, _ => false });
+	assert_eq!(~"cat", substring);
+}
+
+#[test]
+fn test_raw_match_groups() {
+	use pcre::raw::{compile, exec, get_substring, PCRE_NONE};
+
+	let subject = "00:06:08 TAG_1      id=   3,   320,     1,  -321,    11, TAG_END ";
+	let pattern = "TAG_1[\\s]+id=[\\s]*([-\\d]+),[\\s]*([-\\d]+),[\\s]*([-\\d]+),[\\s]*([-\\d]+),[\\s]*([-\\d]+),[\\s]*TAG_END";
+	let regex = match compile(pattern, PCRE_NONE) { Some(r) => r, None => fail!() };
+	let mm = exec(regex, std::ptr::null(), subject, 0, PCRE_NONE, 5);
+	
+	// check substrings
+	let substrings = ~[subject.slice(9, subject.len()-1), "3", "320", "1", "-321", "11"];
+	for i in range(0, 5) {
+		match get_substring(subject, &mm, i as uint) {
+			Some(s)	=> assert_eq!(substrings[i].into_owned(), s),
+			None	=> fail!()
+		}
+	}
 }
 
 #[test]
 fn test_raw_unmatch () {
-	let regex = match compile("cat", pcre::PCRE_NONE) { Some(r) => r, None => { assert!(false); return }};
-	let subject = "dog and bird";
-	let mm = exec(regex, std::ptr::null(), subject, 0, pcre::PCRE_NONE, 1);
+	use pcre::raw::{compile, exec, PCRE_NONE, NoMatch};
 
-	assert!(match mm { pcre::NoMatch => true, _ => false });
+	let regex = match compile("cat", PCRE_NONE) { Some(r) => r, None => fail!() };
+	let subject = "dog and bird";
+	let mm = exec(regex, std::ptr::null(), subject, 0, PCRE_NONE, 1);
+
+	assert!(match mm { NoMatch => true, _ => false });
 }
 
 // test JIT based regex matching
 #[test]
-fn test_jit_match () {
-	let regex = match compile("cat", pcre::PCRE_NONE) { Some(r) => r, None => { assert!(false); return }};
-	let extra = match study(regex, pcre::PCRE_STUDY_JIT_COMPILE) { Some(e) => e, None => { assert!(false); return }};
-	let subject = "dog and cat";
-	let mm = exec(regex, extra, subject, 0, pcre::PCRE_NONE, 1);
-	let substring = match get_substring(subject, &mm, 0) {
-		Some(s)	=> s,
-		None	=> { assert!(false); return }
-	};
-	assert!(match mm { pcre::Match(_,_) => true, _ => false });
-	assert_eq!(~"cat", substring);
+fn test_raw_jit_match1 () {
+	use pcre::raw::{compile, study, exec, get_substring, PCRE_NONE, PCRE_STUDY_JIT_COMPILE, Match};
 
-	let subject = "catch";
-	let mm = exec(regex, extra, subject, 0, pcre::PCRE_NONE, 1);
+	let regex = match compile("cat", PCRE_NONE) { Some(r) => r, None => fail!() };
+	let extra = match study(regex, PCRE_STUDY_JIT_COMPILE) { Some(e) => e, None => fail!() };
+	let subject = "dog and cat";
+	let mm = exec(regex, extra, subject, 0, PCRE_NONE, 1);
 	let substring = match get_substring(subject, &mm, 0) {
 		Some(s)	=> s,
 		None	=> { assert!(false); return }
 	};
-	assert!(match mm { pcre::Match(_,_) => true, _ => false });
+	assert!(match mm { Match(_,_) => true, _ => false });
 	assert_eq!(~"cat", substring);
 }
 
 #[test]
-fn test_jit_unmatch () {
-	let regex = match compile("cat", pcre::PCRE_NONE) { Some(r) => r, None => { assert!(false); return }};
-	let extra = match study(regex, pcre::PCRE_STUDY_JIT_COMPILE) { Some(e) => e, None => { assert!(false); return }};
-	let subject = "dog and bird";
-	let mm = exec(regex, extra, subject, 0, pcre::PCRE_NONE, 1);
+fn test_raw_jit_match2 () {
+	use pcre::raw::{compile, study, exec, get_substring, PCRE_NONE, PCRE_STUDY_JIT_COMPILE, Match};
 
-	assert!(match mm { pcre::NoMatch => true, _ => false });
+	let regex = match compile("cat", PCRE_NONE) { Some(r) => r, None => fail!() };
+	let extra = match study(regex, PCRE_STUDY_JIT_COMPILE) { Some(e) => e, None => fail!() };
+	let subject = "catch";
+	let mm = exec(regex, extra, subject, 0, PCRE_NONE, 1);
+	let substring = match get_substring(subject, &mm, 0) {
+		Some(s)	=> s,
+		None	=> fail!()
+	};
+	assert!(match mm { Match(_,_) => true, _ => false });
+	assert_eq!(~"cat", substring);
+}
+
+#[test]
+fn test_raw_jit_match_groups() {
+	use pcre::raw::{compile, study, exec, get_substring, PCRE_NONE, PCRE_STUDY_JIT_COMPILE};
+
+	let subject = "00:06:08 TAG_1      id=   3,   320,     1,  -321,    11, TAG_END ";
+	let pattern = "TAG_1[\\s]+id=[\\s]*([-\\d]+),[\\s]*([-\\d]+),[\\s]*([-\\d]+),[\\s]*([-\\d]+),[\\s]*([-\\d]+),[\\s]*TAG_END";
+	let regex = match compile(pattern, PCRE_NONE) { Some(r) => r, None => fail!() };
+	let extra = match study(regex, PCRE_STUDY_JIT_COMPILE) { Some(e) => e, None => fail!() };
+	let mm = exec(regex, extra, subject, 0, pcre::raw::PCRE_NONE, 5);
+	
+	// check substrings
+	let substrings = ~[subject.slice(9, subject.len()-1), "3", "320", "1", "-321", "11"];
+	for i in range(0, 5) {
+		match get_substring(subject, &mm, i as uint) {
+			Some(s)	=> assert_eq!(substrings[i].into_owned(), s),
+			None	=> fail!()
+		}
+	}
+}
+
+#[test]
+fn test_raw_jit_unmatch () {
+	use pcre::raw::{compile, study, exec, PCRE_NONE, PCRE_STUDY_JIT_COMPILE, NoMatch};
+
+	let regex = match compile("cat", PCRE_NONE) { Some(r) => r, None => fail!() };
+	let extra = match study(regex, PCRE_STUDY_JIT_COMPILE) { Some(e) => e, None => fail!() };
+	let subject = "dog and bird";
+	let mm = exec(regex, extra, subject, 0, PCRE_NONE, 1);
+
+	assert!(match mm { NoMatch => true, _ => false });
 }
 
 
